30e70d40f36f8342e7cc2a1c7cc7f1bfbc9ce215
mahmoud malek <information.mahmoud@gmail.com>
2024-02-21 22:48:28 +0200

    fixed a typo in place.py

--- models/place.py
+++ models/place.py
@@ -35,7 +35,7 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    aminity_ids = []
+    amenity_ids = []
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review",
