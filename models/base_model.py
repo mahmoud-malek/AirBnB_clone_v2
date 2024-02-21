caec73d6301df3f15c26c5f3a8d819e125ecda32
mahmoud malek <information.mahmoud@gmail.com>
2024-02-21 05:45:42 +0200

    fixed base model that does not handle where not created at or updated at cases

--- models/base_model.py
+++ models/base_model.py
@@ -26,14 +26,18 @@ class BaseModel:
                     setattr(self, key, value)
             if 'id' not in kwargs.keys():
                 setattr(self, 'id', str(uuid.uuid4()))
+            if 'created_at' not in kwargs.keys():
+                setattr(self, 'created_at', datetime.now())
+            if 'updated_at' not in kwargs.keys():
+                setattr(self, 'updated_at', datetime.now())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        return "[{}] ({}) {}".format(type(self).__name__,
-                                     self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
