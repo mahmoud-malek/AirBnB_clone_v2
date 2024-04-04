abed80dfa8e10bd1e8a23bcbf1d72b2e21b5d4ea
mahmoud malek <information.mahmoud@gmail.com>
2024-04-04 23:53:07 +0200

    add all in one file

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,8 +3,76 @@
  that creates and distributes an archive to your web servers,
   using the function deploy:"""
 
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+from fabric.api import local
+from datetime import datetime
+import os
+from fabric.api import run, put, env
+
+env.hosts = ['34.202.158.120', '18.234.145.174']
+
+
+def do_deploy(archive_path):
+    """ this is  a function to deploy the server """
+
+    if not os.path.isfile(archive_path):
+        return False
+
+    archive_name = archive_path.split('/')[1].split('.')[0]
+
+    status = put(archive_path, '/tmp')
+    if status.failed:
+        return False
+
+    status = run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
+    if status.failed:
+        return False
+
+    status = run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'.format(
+        archive_name, archive_name))
+    if status.failed:
+        return False
+
+    status = run('rm /tmp/{}.tgz'.format(archive_name))
+    if status.failed:
+        return False
+
+    status = run('mv /data/web_static/releases/{}/web_static/* \
+    /data/web_static/releases/{}'.format(archive_name, archive_name))
+    if status.failed:
+        return False
+
+    status = run(
+        'rm -rf /data/web_static/releases/{}/web_static'.format(archive_name))
+    if status.failed:
+        return False
+
+    status = run('rm -rf /data/web_static/current')
+    if status.failed:
+        return False
+
+    status = run('ln -s /data/web_static/releases/{} \
+        /data/web_static/current'.format(archive_name))
+    if status.failed:
+        return False
+
+    return True
+
+
+def do_pack():
+    """ A function to pack all files in web_static
+    folder
+    """
+    date_now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = 'versions/web_static_{}.tgz'.format(date_now)
+    local('mkdir -p versions')
+
+    status = local(
+        'tar -cvzf {} web_static'.format(archive_path))
+
+    if status.failed:
+        return None
+    else:
+        return archive_path
 
 
 def deploy():
