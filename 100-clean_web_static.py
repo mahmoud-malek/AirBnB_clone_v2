03bc6f0d31c059c93745805f80976a59c3fbc006
mahmoud malek <information.mahmoud@gmail.com>
2024-04-05 00:52:31 +0200

    100

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,6 +2,17 @@
 """ a fabric script that deletes out-of-date archives,
  using the function do_clean"""
 
+import os
+from fabric.api import local, env, run
 
-def do_clean():
+env.hosts = ['18.234.145.174', '34.202.158.120']
+
+
+def do_clean(number=0):
     """ a function that deletes out-of-date archives"""
+    number = int(number)
+    if number < 2:
+        number = 2
+    local('ls -dt versions/* | tail -n +{} | xargs rm -rf'.format(number))
+    run('ls -dt /data/web_static/releases/* |\
+         tail -n +{} | xargs rm -rf'.format(number))
