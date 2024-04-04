438aa6af05c2524ae7e0a5370157a5b6eed9b3f5
mahmoud malek <information.mahmoud@gmail.com>
2024-04-05 01:19:11 +0200

    there is something strange

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,11 +3,11 @@
  that creates and distributes an archive to your web servers,
   using the function deploy:"""
 
-import os
-from fabric.api import run, put, env, local
 from datetime import datetime
+import os.path
+from fabric.api import put, run, env, local
 
-env.hosts = ['34.202.158.120', '18.234.145.174']
+env.hosts = ['18.234.145.174', '34.202.158.120']
 
 
 def do_pack():
@@ -30,8 +30,13 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """ this is  a function to deploy the server """
-
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
@@ -39,21 +44,22 @@ def do_deploy(archive_path):
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".format(
-            name)).failed is True:
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            file, name)).failed is True:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
         return False
     if run("rm /tmp/{}".format(file)).failed is True:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".format(
-            name)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
@@ -64,10 +70,9 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    """this is a function to fully deploy the website"""
-
-    archive_path = do_pack()
-    if archive_path is None:
-        return None
-
-    return do_deploy(archive_path)
+    """ this is  a function to deploy the server
+         """
+    file = do_pack()
+    if file is None:
+        return False
+    return do_deploy(file)
