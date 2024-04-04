4b462dba86b336a7505ece31154c93406ab3b328
mahmoud malek <information.mahmoud@gmail.com>
2024-04-05 01:04:18 +0200

    try

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -30,47 +30,34 @@ def do_pack():
 def do_deploy(archive_path):
     """ this is  a function to deploy the server """
 
-    if not os.path.isfile(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    archive_name = archive_path.split('/')[1].split('.')[0]
-
-    status = put(archive_path, '/tmp')
-    if status.failed:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-
-    status = run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
-    if status.failed:
+    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
         return False
-
-    status = run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'.format(
-        archive_name, archive_name))
-    if status.failed:
+    if run("mkdir -p /data/web_static/releases/{}/".format(
+            name)).failed is True:
         return False
-
-    status = run('rm /tmp/{}.tgz'.format(archive_name))
-    if status.failed:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+            file, name)).failed is True:
         return False
-
-    status = run('mv /data/web_static/releases/{}/web_static/* \
-    /data/web_static/releases/{}'                                 .format(archive_name, archive_name))
-    if status.failed:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
-
-    status = run(
-        'rm -rf /data/web_static/releases/{}/web_static'.format(archive_name))
-    if status.failed:
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-
-    status = run('rm -rf /data/web_static/current')
-    if status.failed:
+    if run("rm -rf /data/web_static/releases/{}/web_static".format(
+            name)).failed is True:
         return False
-
-    status = run('ln -s /data/web_static/releases/{} \
-        /data/web_static/current'                                 .format(archive_name))
-    if status.failed:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
-
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
     return True
 
 
