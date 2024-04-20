8ca8df5a755b0c2f170cba4d1d1d60d1766fc442
mahmoud malek <information.mahmoud@gmail.com>
2024-04-20 21:21:13 +0200

    add\udpate files :fire:

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,8 +23,9 @@ def c_text(text):
     return "C {}".format(text)
 
 
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def c_text(text="is cool"):
+def python_text(text):
     text = text.replace('_', ' ')
     return "Python {}".format(text)
 
