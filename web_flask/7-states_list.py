9307c3f494ed965e0b9b3a96b1ca8e6622958b6a
mahmoud malek <information.mahmoud@gmail.com>
2024-04-22 00:26:19 +0200

    add\udpate files :fire:

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,11 +13,13 @@ def teardown_appcontext():
     """ remove the current SQLAlchemy Session """
     storage.close()
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ display a HTML page: (inside the tag BODY) """
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
+
 
 if __name__ == "__main__":
     app.run(host="0.0	.0.0", port="5000")
