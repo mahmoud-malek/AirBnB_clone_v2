63fbb22f674376b6671ffbad97a3d479f8849524
mahmoud malek <information.mahmoud@gmail.com>
2024-04-21 09:41:24 +0200

    add\udpate files :fire:

--- models/state.py
+++ models/state.py
@@ -7,18 +7,18 @@ import models
 
 
 class State(BaseModel, Base):
-	""" State class """
-	__tablename__ = 'states'
-	name = Column(String(128), nullable=False)
-	cities = relationship("City", backref="state", cascade="all, delete")
+    """ State class """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
 
-	@property
-	def cities(self):
-		storage = models.storage.all()
-		result = []
-		for key in storage:
-			city = key.replace('.',' ')
-			city = city.split(' ')
-			if (city[0] == 'City' and storage[key].state_id == self.id):
-				result.append(storage[key])
-		return (result)
+    @property
+    def cities(self):
+        storage = models.storage.all()
+        result = []
+        for key in storage:
+            city = key.replace('.', ' ')
+            city = city.split(' ')
+            if (city[0] == 'City' and storage[key].state_id == self.id):
+                result.append(storage[key])
+        return (result)
